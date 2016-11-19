dbdcf5f3ebf9f4fdfd340a7570fa1f6988d54794
elana tee <elanatee@gmail.com>
2016-11-19 16:30:48 -0500

    cleaning up

--- views.py
+++ views.py
@@ -1,10 +1,9 @@
-url = ''
-
 from flask import Flask
 from flask import render_template
 from flask import request
 import scraper
 import demo
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,13 +12,17 @@ def home():
         'index.html'
     )
 
-@app.route('/next', methods=['POST'])
+@app.route('/quote', methods=['POST'])
 def next():
     if request.method == 'POST':
         url = request.form['address']
         tag = demo.getTag(url)
         quote = scraper.scrape(tag)
+        # TODO: failure page
         return render_template(
-            'next.html', 
-            greeting=quote
+            'quote.html', 
+            quote=quote
+        )
+    return render_template(
+            'index.html'
         )
